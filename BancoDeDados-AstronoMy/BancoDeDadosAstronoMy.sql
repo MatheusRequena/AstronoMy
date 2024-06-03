42303b589596f1a262b921c8462c8c56a0b163cd
MatheusRequena <matheus.requena@sptech.school>
2024-06-03 10:23:47 -0300

    Banco de Dados: Mudança de regra de negócio

--- BancoDeDados-AstronoMy/BancoDeDadosAstronoMy.sql
+++ BancoDeDados-AstronoMy/BancoDeDadosAstronoMy.sql
@@ -11,102 +11,52 @@ nome varchar(100),
 email varchar(320),
 telefone char(11),
 dtNasc date,
-senha varchar(30),
-dtCadastro datetime,
-fkAmigo int
+senha varchar(100),
+dtCadastro datetime
 );
 
-CREATE TABLE Forum (
-idForum int primary key auto_increment,
-nome varchar(50),
-descricao varchar(250),
-dtCriacao datetime
+CREATE TABLE Estudo (
+idEstudo int primary key auto_increment,
+nome varchar(100),
+tema varchar(100),
+qtdPergunta int
 );
 
-CREATE TABLE Participantes (
+CREATE TABLE Notas (
+tentativa int,
 fkUsuario int,
-fkForum int,
-primary key(fkUsuario , fkForum),
-posicao varchar(13),
-CONSTRAINT chkPosicao check(posicao IN('Criador' , 'Administrador' , 'Comum'))
-);
-
-CREATE TABLE Comentario (
-idComentario int,
-fkUsuario int,
-fkForum int,
-comentario varchar(1000),
-horario datetime
+fkEstudo int,
+primary key(tentativa , fkUsuario , fkEstudo),
+acertos int
 );
 
 
 
 -- CONFIGURAÇÃO DAS CHAVES ESTRANGEIRAS
-ALTER TABLE Usuario
-	ADD CONSTRAINT fkUsuarioAmigo foreign key (fkAmigo)
-		REFERENCES Usuario (idUsuario);
-        
-ALTER TABLE Participantes
-	ADD CONSTRAINT fkUsuarioParticipante foreign key (fkUsuario)
+ALTER TABLE Notas
+	ADD CONSTRAINT fkUsuarioNota foreign key (fkUsuario)
 		REFERENCES Usuario (idUsuario);
 
-ALTER TABLE Participantes
-	ADD CONSTRAINT fkForumParticipante foreign key (fkForum)
-		REFERENCES Forum (idForum);
-        
-ALTER TABLE Comentario
-	ADD CONSTRAINT fkUsuarioComentario foreign key (fkUsuario)
-		REFERENCES Usuario (idUsuario);
+ALTER TABLE Notas
+	ADD CONSTRAINT fkEstudoNotas foreign key (fkEstudos)
+		REFERENCES Estudo (idEstudo);
 
-ALTER TABLE Comentario
-	ADD CONSTRAINT fkForumComentario foreign key (fkForum)
-		REFERENCES Forum (idForum);
         
         
-
 -- VISUALIZAÇÃO DE DADOS DAS TABELAS
 -- VISUALIZAÇÃO INDIVIDUAL
 SELECT * FROM Usuario;
-SELECT * FROM Forum;
-SELECT * FROM Participantes;
-SELECT * FROM Comentario;
-
--- VISUALIZAR DADDOS DOS USUÁRIOS E DE SEUS RESPECTIVOS AMIGOS
-SELECT * FROM Usuario
-	JOIN Usuario as Amigo
-		ON Usuario.fkAmigo = Amigo.idUsuario;
+SELECT * FROM Estudo;
+SELECT * FROM Notas;
         
--- VISUALIZAR DADOS DOS FÓRUNS E SEUS PARTICIPANTES
-SELECT * FROM Forum
-	JOIN Participantes
-		ON fkForum = idForum;
+-- VISUALIZAR DADOS DOS ESTUDOS E SUAS NOTAS
+SELECT * FROM Estudo
+	JOIN Notas
+		ON fkEstudo = idEstudo;
         
--- SELECIONAR DADOS DOS FÓRUNS E DADOS DE SEUS PARTICIPANTES
-SELECT * FROM Forum
-	JOIN Participantes
-		ON fkForum = idForum
+-- SELECIONAR DADOS DOS ESTUDOS E DADOS DOS USUARIOS QUE ESTUDARAM
+SELECT * FROM Estudo
+	JOIN Notas
+		ON fkForum = idEstudo
 	JOIN Usuario
 		ON fkUsuario = idUsuario;
-        
--- SELECIONAR DADOS DOS USUÁRIOS E SEUS COMENTÁRIOS
-SELECT * FROM Usuario
-	JOIN Participantes
-		ON Participantes.fkUsuario = idUsuario
-	JOIN Comentario
-		ON Comentario.fkUsuario = idUsuario;
-        
--- SELECIONAR DADOS DOS FÓRUNS E SEUS COMENTÁRIOS
-SELECT * FROM Forum
-	JOIN Participantes
-		ON Participantes.fkForum = idForum
-	JOIN Comentario
-		ON Comentario.fkForum = idForum;
-        
--- SELECIONAR DADOS DOS FÓRUNS, DE SEUS PARTICIPANTES E SEUS COMENTÁRIOS
-SELECT * FROM Forum
-	JOIN Participantes
-		ON Participantes.fkForum = idForum
-	JOIN Usuario
-		ON fkUsuario = idUsuario
-	JOIN Comentario
-		ON Comentario.fkForum = idForum;
