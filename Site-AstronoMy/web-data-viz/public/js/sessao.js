eaed025ce031f0a02571616d919d50e067f12a7d
MatheusRequena <matheus.requena@sptech.school>
2024-06-06 12:54:15 -0300

    Site: Atualização da Header

--- Site-AstronoMy/web-data-viz/public/js/sessao.js
+++ Site-AstronoMy/web-data-viz/public/js/sessao.js
@@ -1,36 +1,43 @@
-// sessão
+// Verificar login
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var b_usuario = document.getElementById("b_usuario");
+    
 
     if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
+        header.innerHTML = `<a href="index.html"><img src="img/LogoAstronoMy.png" alt="Logo AstronoMy" class="logo"></a>
+        <a href="index.html" class="${index}">Inicio</a>
+        <a href="EstudarAstronoMy.html" class="${estudar}">Estudar</a>
+        <select id="selectPerfil" onchange="selecionar()">
+        <option value="#" hidden selected>${nome} ‎</option>
+        <option value="perfil">Perfil</option>
+        <option value="sair">Sair</option>
+        `;
+
+        homeEstudar.innerHTML = `<label class="tituloEstudar">TUDO PRONTO</label>
+        <span>Não perca tempo, vamos estudar!!</span>
+        <div class="botoes">
+            <button onclick="estudo()">ESTUDAR</button>
+        </div>`;
     } else {
-        window.location = "../login.html";
+        header.innerHTML = `<a href="index.html"><img src="img/LogoAstronoMy.png" alt="Logo AstronoMy" class="logo"></a>
+        <a href="index.html" class="${index}">Inicio</a>
+        <a href="CadastroAstronoMy.html" class="${cadastro}">Cadastre-se</a>
+        <a href="LoginAstronoMy.html" class="${login}">Entrar</a> 
+    </select>`
+
+    homeEstudar.innerHTML = `<label class="tituloEstudar">ESTUDE CONOSCO</label>
+    <span>Para estudar, não esqueça de se cadastrar!</span>
+    <div class="botoes">
+        <button onclick="entrar()">ENTRAR</button>
+        <button onclick="cadastrar()">CADASTRAR</button>
+    </div>`
     }
 }
 
+// Deslogar
 function limparSessao() {
     sessionStorage.clear();
-    window.location = "../login.html";
+    window.location = "../index.html";
 }
-
-// carregamento (loading)
-function aguardar() {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "flex";
-}
-
-function finalizarAguardar(texto) {
-    var divAguardar = document.getElementById("div_aguardar");
-    divAguardar.style.display = "none";
-
-    var divErrosLogin = document.getElementById("div_erros_login");
-    if (texto) {
-        divErrosLogin.style.display = "flex";
-        divErrosLogin.innerHTML = texto;
-    }
-}
-
